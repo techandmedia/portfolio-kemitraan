6391f90961f1104cb329d7cd88143790c99a492f
techandmedia <eko.andri@icloud.com>
2018-08-11 16:11:41 +0100

    add export config to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /* eslint-disable */
-const withLess = require('@zeit/next-less')
+const withLess = require("@zeit/next-less");
 
 module.exports = withLess({
   lessLoaderOptions: {
-    javascriptEnabled: true,
+    javascriptEnabled: true
   },
-})
+  exportPathMap: function() {
+    return {
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+      "/table1": { page: "/table1" }
+      // '/p/learn-nextjs': { page: '/post', query: { title: "Learn Next.js is awesome" } },
+      // '/p/deploy-nextjs': { page: '/post', query: { title: "Deploy apps with Zeit" } }
+    };
+  }
+});
--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,6 @@ import Layout from './components/layout'
 
 export default () => (
   <Layout>
-    <p>This is the Home Page</p>
+    <p>This is the about page</p>
   </Layout>
 )
